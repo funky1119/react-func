fd02e5808e2f29f35913bc892a388bf2ca61f02e
funky1119 <funky1119@naver.com>
2023-11-09 00:32:20 +0900

    파일 초기 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Todo } from "./screens/Todo";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
