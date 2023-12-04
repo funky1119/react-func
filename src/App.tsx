0670faf6d55fe380d87f97b66c0c50ec20da8d3d
funky1119 <funky1119@naver.com>
2023-12-05 01:14:04 +0900

    styled components Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from "react";
-import Circle from "./screens/Circle/Circle";
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function App() {
+  const Container = styled.div`
+    background-color: ${(props) => props.theme.bgColor};
+  `;
+
+  const H1 = styled.h1`
+    color: ${(props) => props.theme.textColor};
+  `;
+
   return (
-    <div>
-      <Circle bgColor={"teal"} borderColor="black" />
-      <Circle bgColor={"tomato"} text={"그렇다고 한다"} />
-    </div>
+    <Container>
+      <H1>proc</H1>
+    </Container>
   );
 }
 
